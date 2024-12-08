53bd59cbcdb56a3683ebb430696ac1694aa20dbf
Muhamed Abu Al-Kibash <w0801109@myscc.ca>
2024-12-07 21:09:44 -0500

    Home page is done

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,40 +1,36 @@
-import Link from "next/link"
-import { Button } from "./ui/button"
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 // components
-import Nav from "./Nav"
-import MobileNav from "./MobileNav"
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Muhamed<span className="text-accent">.</span>
+          </h1>
+        </Link>
 
-            {/* logo */}
-            <Link href="/">
-                <h1 className="text-4xl font-semibold">
-                    Muhamed<span className="text-accent">.</span>
-                </h1>
-            </Link>
-
-            {/* desktop nav & */}
-            <div className="hidden xl:flex items-center gap-8">
-                <Nav />
-                <Link href="/contact">
-                    <Button>Hire Me</Button>
-                </Link>
-            </div>
-
-            {/* mobile nav */}
-            <div className="xl:hidden">
-                <MobileNav />
-            </div>
-
-
-
+        {/* desktop nav & */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire Me</Button>
+          </Link>
         </div>
-    </header>
-  ) 
-}
 
-export default Header
+        {/* mobile nav */}
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
